81f26ffd1e8def1d2cb289774cc55943cecc3d93
xhs <xuhuasong@eastmoney.com>
2024-12-25 19:46:51 +0800

    about code analyze

--- src/server.h
+++ src/server.h
@@ -1415,6 +1415,7 @@ struct redisServer {
     unsigned int maxclients;            /* Max number of simultaneous clients */
     unsigned long long maxmemory;   /* Max number of memory bytes to use */
     int maxmemory_policy;           /* Policy for key eviction */
+    // lru淘汰的采样数
     int maxmemory_samples;          /* Precision of random sampling */
     int lfu_log_factor;             /* LFU logarithmic counter factor. */
     int lfu_decay_time;             /* LFU counter decay factor. */
